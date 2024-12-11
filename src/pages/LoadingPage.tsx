522767f730207136c76eea20fcf5897e1310ebab
Santiago <santisegurado1@gmail.com>
2024-12-10 21:22:22 -0300

    Add LoadingComponent and LoadingPage to improve loading experience

---
+++ src/pages/LoadingPage.tsx
@@ -0,0 +1,15 @@
+import LoadingComponent from "../components/ui/LoadingComponent";
+import RedAtlasBrand from "../assets/logo-red-atlas.png";
+
+const LoadingPage = () => {
+  return (
+    <section className="w-full h-screen flex items-center justify-center">
+      <div className="relative">
+      <img src={RedAtlasBrand} alt="Red Atlas Brand" width={50} height={50}/>
+        <LoadingComponent className="w-28 h-28 absolute -top-6 -right-8" />
+    </div>
+    </section>
+  );
+};
+
+export default LoadingPage;
