a74378d0862544cadbd8e5f8d3c12ad87e42a61b
Jableed43 <javiernehuenlopez@gmail.com>
2024-12-15 21:12:08 -0300

    completo con evns

--- code/src/components/MapComponent.tsx
+++ code/src/components/MapComponent.tsx
@@ -1,6 +1,8 @@
 import { FunctionComponent } from 'react';
 import { GoogleMap, MarkerF, useLoadScript } from '@react-google-maps/api';
 
+const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
+
 interface Location {
   lat: number;
   lng: number;
@@ -13,7 +15,7 @@ interface MapComponentProps {
 
 const MapComponent: FunctionComponent<MapComponentProps> = ({ location }) => {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyDDeGrrX8vGoLru1jtM88kVMO9vWHh2Ong",
+    googleMapsApiKey: GOOGLE_API_KEY,
   });
 
   if (!isLoaded) return <div>Cargando mapa...</div>;
